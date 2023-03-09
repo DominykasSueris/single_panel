8a69a5e943a804a2aa1ca8570cc8adfe9f87d32a
Dominykas Sueris <dominykassueris@gmail.com>
2023-03-09 21:06:39 +0000

    code formated prettier

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,8 @@
 module.exports = {
-     clearMocks: true,
-     collectCoverage: true,
-     coverageDirectory: "coverage",
-     coverageProvider: "v8",
-     testEnvironment: "jsdom",
-     testMatch: [
-          "**/__tests__/**/*.[jt]s?(x)",
-          "**/?(*.)+(test).[tj]s?(x)"
-        ],
-}
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  testEnvironment: "jsdom",
+  testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(test).[tj]s?(x)"]
+};
