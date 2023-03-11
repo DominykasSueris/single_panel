cde3c0cb8c204f4922087d4ddd7b40fac0e53a66
Dominykas Sueris <dominykassueris@gmail.com>
2023-03-11 19:44:51 +0000

    searchDate redux state deleted Ref #37

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,14 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "redux/reducers/auth";
 import logsSlice from "redux/reducers/logs";
-import dateSlice from "redux/reducers/searchDate";
+// import dateSlice from "redux/reducers/searchDate";
 import loadingSlice from "redux/reducers/loading";
 
 export const store = configureStore({
   reducer: {
     auth: authSlice,
     logs: logsSlice,
-    date: dateSlice,
     loading: loadingSlice
   }
 });
