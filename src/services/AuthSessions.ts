767236388bde0f5a4ba2f25bde01e03d1688cabe
Dominykas Sueris <dominykassueris@gmail.com>
2023-03-15 16:32:25 +0000

    Auth session methods changes Ref #47

--- src/services/AuthSessions.ts
+++ src/services/AuthSessions.ts
@@ -1,24 +1,34 @@
-import { LoginConfig } from "components/Auth/Login";
+import { Session } from "components/Auth/Login";
+import { CloudWatch } from "./aws/aws";
 
 export class AuthSessions {
   static session_methods_key = "auth_methods";
 
-  static getMethods(): LoginConfig[] {
+  static getMethods(): Session[] {
     const sessionMethods = window.sessionStorage.getItem(this.session_methods_key);
     if (!sessionMethods) return [];
     const methods = [...JSON.parse(sessionMethods)];
     return methods;
   }
 
-  static setMethods(methods: LoginConfig[]) {
+  static setMethods(methods: Session[]) {
     const sessionStorage = window.sessionStorage;
     const methodsString = JSON.stringify(methods);
     sessionStorage.setItem(this.session_methods_key, methodsString);
   }
 
-  static updateMethods(method: LoginConfig) {
+  static updateMethods(method: Session) {
     const sessionMethods = this.getMethods();
     sessionMethods.push(method);
     this.setMethods(sessionMethods);
+  }
+
+  static deleteMethods(tag: string) {
+    const existingMethods = AuthSessions.getMethods();
+    const remainingConnections = existingMethods.filter(
+      existingMethod => existingMethod.tag !== tag
+    );
+    CloudWatch.removeWatcher(tag);
+    AuthSessions.setMethods(remainingConnections);
   }
 }
